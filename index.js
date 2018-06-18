8d56ba827a2841286abc032ad3d8e2473cc77c91
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-06-18 20:11:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = 'space';
 
-var sentence = 'Let\s talk about';
+var sentence = 'Let\'s talk about';
 
 var conversation = `${sentence} ${topic}`;
